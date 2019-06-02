2cb36ac38200f9080d7dfe993956cbabe47a8cea
ben <trewelb@gmail.com>
2019-06-02 21:23:28 +0100

    done websites and finished video games

--- common.js
+++ common.js
@@ -11,6 +11,13 @@ window.addEventListener("load", () =>
     //this image
     for(var img of imgs)
         img.addEventListener("click", onImageClick.bind(img));
+
+
+    //When the close button is clicked
+    document.querySelector("#close-image-button").addEventListener("click", () =>
+    {
+        document.querySelector("image-viewer").style.display = "none";
+    });
 });
 
 /**
@@ -21,6 +28,22 @@ function onImageClick()
     //Get the image source
     let src = this.src;
 
-    //Just set the location to this src
-    window.location.href = src;    
+    //Get the caption and title
+    let caption = this.getAttribute("data-caption");
+    let title   = this.getAttribute("data-title");
+
+    //First get the image viewer elements
+    let titleElement    = document.querySelector("image-viewer h1");
+    let captionElement  = document.querySelector("image-viewer p");
+    let figure          = document.querySelector("image-viewer figure");
+
+    //Set the title and caption
+    titleElement.textContent = title;
+    captionElement.textContent = caption;
+    
+    //Set background image
+    figure.style.backgroundImage = "url(" + src + ")";
+
+    //Show the image viewer
+    document.querySelector("image-viewer").style.display = "block";
 }
